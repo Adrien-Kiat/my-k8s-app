a04cbe294a552c2c3d81ea5db77ab72e2f69d9e8
Adrien-Kiat <2153168@cegepmontpetit.ca>
2025-04-30 11:24:05 -0400

    add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('daph\n');
+    res.end(' <3 daph\n');
 });
 
 
