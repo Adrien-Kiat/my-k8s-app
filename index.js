7ac3df8ec4434652a00828329a122c536719413d
Adrien-Kiat <2153168@cegepmontpetit.ca>
2025-05-02 10:43:11 -0400

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(' <change plz\n');
+    res.end(' <test test test\n');
 });
 
 
